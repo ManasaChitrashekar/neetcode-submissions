3b5804c070626b738200f4f3d3b48e4abc5f1ce2
ManasaChitrashekar <manasa.ncm7@gmail.com>
2026-04-21 17:59:56 -0400

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n ==1:
+            return 1
+        if n ==2 :
+            return 2
+        n1,n2 = 1,2
+        for i in range(3,n+1):
+            res = n2 + n1
+            n1,n2 = n2,res
+        return n2
