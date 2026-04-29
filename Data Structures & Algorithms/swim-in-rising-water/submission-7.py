f7d4c78ec96f59abce64ad2a728911db885805bb
ManasaChitrashekar <manasa.ncm7@gmail.com>
2026-04-29 08:17:50 -0400

    Add: swim-in-rising-water - submission-7

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        ROWS ,COLS = len(grid),len(grid[0])
+        minheap = []
+        heapq.heappush(minheap,(grid[0][0],0,0)) 
+        #height , r,c
+        visited = set()
+        visited.add((0,0))
+        dir = [[1,0],[-1,0],[0,1],[0,-1]]
+        while minheap:
+            time,r,c = heapq.heappop(minheap)
+            if (r,c)==(ROWS-1,COLS-1):
+                return time
+            for m,n  in  dir:
+                rm,cn = m+r,n+c
+                if 0 <= rm < ROWS and 0 <= cn < COLS and (rm,cn) not in visited:
+                    visited.add((rm,cn))
+                    heapq.heappush(minheap,(max(time,grid[rm][cn]),rm,cn))
+                    
+        
+
