943a3911da5d7f67fae492a71b4489767edcfaa4
ManasaChitrashekar <manasa.ncm7@gmail.com>
2026-06-08 16:46:08 -0400

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m,n = len(word1),len(word2)
+        cache = [[float("-inf")] * (n+1) for _ in range(m+1)]
+        p = n
+        for i in range(n+1):
+            cache[m][i]=p
+            p -=1
+        p = m
+        for j in range(m+1):
+            cache[j][n]=p
+            p -=1
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if word1[i]==word2[j]:
+                    cache[i][j]=cache[i+1][j+1]
+                else:
+                    cache[i][j] = 1+min(cache[i+1][j],cache[i][j+1],cache[i+1][j+1])
+        
+        return cache[0][0]
